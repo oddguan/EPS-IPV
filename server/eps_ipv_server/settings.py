b81889dc61e5671c0a7b560871ddd5b02da891f5
Chenxiao Guan <1011zaozao@gmail.com>
2020-03-20 23:55:41 -0400

    modified server so that it serves frontend app

--- server/eps_ipv_server/settings.py
+++ server/eps_ipv_server/settings.py
@@ -13,8 +13,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../'))
 
+REACT_APP_DIR = os.path.join(BASE_DIR, 'client')
+
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build', 'static'),
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
