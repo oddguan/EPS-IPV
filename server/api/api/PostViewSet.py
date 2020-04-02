8067d81d01715b8670e97ffb5704e0bfdc54790d
Chenxiao Guan <1011zaozao@gmail.com>
2020-04-01 20:36:47 -0400

    formatting

--- server/api/api/PostViewSet.py
+++ server/api/api/PostViewSet.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, permissions
 from api.models import Post
 from api.serializers import PostSerializer
 
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     permission_classes = [
