6fd62ddf6b2fddfa37cf68ad317712d97cba0aa0
Chenxiao Guan <1011zaozao@gmail.com>
2020-04-02 17:28:01 -0400

    reimplemented victim registration api

---
+++ server/api/api/auth/RegisterAPI.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from api.serializers import VictimRegisterRequestSerializer, VictimRegisterResponseSerializer
+from api.models import Victim
+
+
+class VictimRegisterAPI(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = VictimRegisterRequestSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        account = get_user_model().objects.create_account(
+            serializer.data['username'],
+            serializer.data['password'],
+            serializer.data['hint'],
+            'a random pk'
+        )
+        account.save()
+
+        victim = Victim(account=account)
+        victim.save()
+
+        return Response({
+            'victim': VictimRegisterResponseSerializer(
+                account,
+                context=self.get_serializer_context()
+            ).data,
+            'accessToken': str(RefreshToken.for_user(account).access_token)
+        })
---
+++ server/api/api/auth/__init__.py
@@ -0,0 +1,1 @@
+from .RegisterAPI import VictimRegisterAPI
