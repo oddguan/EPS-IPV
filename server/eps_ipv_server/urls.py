9cf3abc46cfdfe2bc8ed7dcff946591fa59bd882
Chenxiao Guan <1011zaozao@gmail.com>
2020-04-07 13:32:15 -0400

    add swagger support

--- server/eps_ipv_server/urls.py
+++ server/eps_ipv_server/urls.py
@@ -6,9 +6,30 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="EPS IPV API Documentation",
+        default_version='v1',
+        description="This is the backend API Documentation of the Intimate Partner Violence Project",
+        contact=openapi.Contact(email="chenxiag@andrew.cmu.edu"),
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger',
+                                           cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc',
+                                         cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
