2a8324746543cb764fe2ac269d422a0aa8dafcb7
Chenxiao Guan <1011zaozao@gmail.com>
2020-04-07 17:44:30 -0400

    image upload WIP

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework import routers
 from .api import VictimViewSet, PostViewSet, LogViewSet
 from api.api.auth import VictimRegisterAPI, LoginAPI
+from api.api import ImageUploadAPI
 
 
 router = routers.DefaultRouter()
@@ -13,7 +14,9 @@ router.register(r'log', LogViewSet, basename='log')
 urlpatterns = [
     path(r'auth/register/victim', VictimRegisterAPI.as_view(),
          name='register victim account'),
-    path(r'auth/login', LoginAPI.as_view(), name='login')
+    path(r'auth/login', LoginAPI.as_view(), name='login'),
+
+    path(r'image', ImageUploadAPI.as_view(), name='image-upload')
 ]
 
 urlpatterns += router.urls
