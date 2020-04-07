f49cc35def40fa64f063cc62b5e37ef4885f706a
Chenxiao Guan <1011zaozao@gmail.com>
2020-04-07 15:14:58 -0400

    add new log wysiwyg editor

---
+++ client/src/components/Logs/NewLog/MyEditor/MyEditor.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: '90%',
+    border: '1px solid #eee',
+    margin: theme.spacing(1),
+    padding: theme.spacing(1),
+  },
+  titleField: {
+    margin: theme.spacing(1),
+    padding: theme.spacing(1),
+    border: '1px solid #ccc',
+  },
+  titleInput: {
+    width: '80%',
+    margin: 'auto',
+  },
+  editor: {
+    border: '1px solid #ccc',
+    height: 'auto',
+  },
+}));
+
+const MyEditor = () => {
+  const classes = useStyles();
+
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (newEditorState) => {
+    setEditorState(newEditorState);
+  };
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.titleField}>
+        <InputLabel htmlFor='new-log-title'>Title</InputLabel>
+        <TextField className={classes.titleInput} id='new-log-title' />
+      </div>
+      <div className={classes.editor}>
+        <Editor
+          editorState={editorState}
+          wrapperClassName='demo-wrapper'
+          editorClassName='demo-editor'
+          onEditorStateChange={onEditorStateChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MyEditor;
--- client/src/components/Logs/NewLog/NewLog.js
+++ client/src/components/Logs/NewLog/NewLog.js
@@ -1,32 +1,45 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { push } from 'connected-react-router';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
+import MyEditor from './MyEditor/MyEditor';
+
+const useStyles = makeStyles((theme) => ({
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
     [theme.breakpoints.up('sm')]: {
       width: 'calc(100% - 240px)',
-      marginLeft: 240
-    }
+      marginLeft: 240,
+    },
   },
   toolbar: theme.mixins.toolbar,
   flex: {
     display: 'flex',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
   },
-  fab: {
-    position: 'fixed',
-    bottom: theme.spacing(2),
-    right: theme.spacing(2)
-  }
+  buttonsWrapper: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    width: '85%',
+  },
+  button: {
+    width: 150,
+  },
 }));
 
 const Logs = () => {
   const classes = useStyles();
+
+  const dispatch = useDispatch();
+  const goBackToLogsPage = () => {
+    dispatch(push('/logs'));
+  };
 
   return (
     <React.Fragment>
@@ -35,6 +48,24 @@ const Logs = () => {
         <CssBaseline />
         <Typography variant='h5'>Add New Log</Typography>
         <Divider />
+        <MyEditor />
+        <div className={classes.buttonsWrapper}>
+          <Button
+            className={classes.button}
+            variant='contained'
+            color='secondary'
+            onClick={goBackToLogsPage}
+          >
+            Cancel
+          </Button>
+          <Button
+            className={classes.button}
+            variant='contained'
+            color='primary'
+          >
+            Save New Log
+          </Button>
+        </div>
       </div>
     </React.Fragment>
   );
