8c1a094a66c248619bb66797949e09ac76ee38c6
Chenxiao Guan <1011zaozao@gmail.com>
2020-04-08 00:14:18 -0400

    turning debug flag to true for fixing error

--- server/eps_ipv_server/settings.py
+++ server/eps_ipv_server/settings.py
@@ -48,7 +48,7 @@ STATICFILES_DIRS = [
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['protect-ipv-victims.xyz', '18.223.244.40', 'localhost']
 
