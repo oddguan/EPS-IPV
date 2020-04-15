2bb545710c58215616b49d1bc09e395e2708ec17
Chenxiao Guan <1011zaozao@gmail.com>
2020-04-15 02:19:39 -0400

    added back deleted serving frontend code

--- server/eps_ipv_server/urls.py
+++ server/eps_ipv_server/urls.py
@@ -41,4 +41,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    url(r'^', views.FrontendAppView.as_view()),
 ]
